ee8330c3750ca54325b837944dfdaa12ffc52626
Dimitri <dimitri.guitton.dev@gmail.com>
2023-04-15 17:59:58 +0200

    feat: Exo 2 - useEffect

--- src/exercise/2-use-effect/02.jsx
+++ src/exercise/2-use-effect/02.jsx
@@ -1,40 +1,76 @@
-// 🦁 Ajout l'import de useEffect
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// 🦁 Crée une variable `NAME_KEY` avec la valeur `name`
+const getInitialName = (key, defaultValue) => {
+    return JSON.parse(localStorage.getItem(key)) || defaultValue;
+};
+
+const useStickyState = (key, defaultValue) => {
+    const [state, setState] = useState(() => getInitialName(key, defaultValue));
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state));
+    }, [state, key]);
+
+    return [state, setState];
+};
+
+const NAME_KEY = 'name';
 
 const NameInput = ({ defaultValue }) => {
-  // 🦁 Change la valeur par défaut en récupérant la valeur stockée dans le localStorage
-  // 💡 JSON.parse(localStorage.getItem(key))
-  // 🦁 Attention il faut vérifier que localStorage.getItem(key) n'est pas null sinon tu vas avoir une erreur !
-  // Si il est vide, tu peux utiliser la valeur par défaut
-  const [name, setName] = useState(defaultValue);
+    const [name, setName] = useStickyState(NAME_KEY, defaultValue);
 
-  // 🦁 Dans un `useEffect` update la valeur stockée dans le localStorage.
-  // 💡 localStorage.setItem(key, JSON.stringify(name));
+    return (
+        <label className="textfield">
+            Name
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+        </label>
+    );
+};
 
-  return (
-    <label className="textfield">
-      Name
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-    </label>
-  );
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+    const [isWindowIncrementActive, setIsWindowIncrementActive] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            console.log('%c HANDLE RESIZE', 'background: #fdd835; color: #000000');
+            setCounter(current => current + 1);
+        };
+
+        if (isWindowIncrementActive) {
+            window.addEventListener('resize', handleResize);
+        }
+
+        return () => {
+            // Clean UP
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [isWindowIncrementActive]);
+
+    return (
+        <>
+            <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+            <input
+                type="checkbox"
+                checked={isWindowIncrementActive}
+                onChange={e => setIsWindowIncrementActive(e.target.checked)}
+            />
+        </>
+    );
+
 };
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  return (
-    <div className="vertical-stack">
-      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
-
-      <NameInput defaultValue="" />
-    </div>
-  );
+    return (
+        <div className="vertical-stack">
+            <Counter />
+            <NameInput defaultValue="" />
+        </div>
+    );
 };
 
 export default App;
