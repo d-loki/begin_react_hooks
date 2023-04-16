bef50f9b58cb8f407aac369d5941a3cad37d4055
Dimitri <dimitri.guitton.dev@gmail.com>
2023-04-16 09:56:53 +0200

    feat: Exo 3 - useRef

--- src/exercise/3-use-ref/03.jsx
+++ src/exercise/3-use-ref/03.jsx
@@ -1,56 +1,65 @@
-/* eslint-disable no-unused-vars */ // 🦁 Enlève cette ligne
-import { useState } from 'react';
+/* eslint-disable no-unused-vars */                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                // 🦁 Enlève cette ligne
+import { useEffect, useRef, useState } from 'react';
 
 const useDebounce = (callback, time) => {
-  // 🦁 Remplace cette variable par un `useRef`
-  // 💡 timeout correspond à la référence de notre timeout.
-  //   Quand tu fais un setTimeout, il return une valeur que
-  //   tu peux clear afin de l'annuler. https://developer.mozilla.org/fr/docs/Web/API/setTimeout#valeur_de_retour
-  const timeout = null;
+    const timeoutRef = useRef(null);
 
-  const onDebounce = (...args) => {
-    // 🦁 Annule le timeout https://developer.mozilla.org/en-US/docs/Web/API/clearTimeout
-    // ℹ️ Cette fonction sera appelée à chaque fois que l'user tape un caractère, on veut donc clear
-    //    le dernier timeout pour relancer un nouveau timeout.
-    // 🦁 Crée un nouveau timeout https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
-    //    a la fin il doit appeler la callback avec les arguments et le temps est défini par le paramètre `time`
-  };
+    const onDebounce = (...args) => {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => {
+            callback(...args);
+        }, time);
+    };
 
-  return onDebounce;
+    return onDebounce;
+};
+
+const useRenderCount = () => {
+    const counter = useRef(0);
+
+    useEffect(() => {
+        console.log('%c Call on any render', 'background: #fdd835; color: #000000');
+        counter.current++;
+    });
+
+    return counter.current;
+
 };
 
 const fetchAgeByName = (name) => {
-  return fetch(`https://api.agify.io/?name=${name}`).then((res) => res.json());
+    return fetch(`https://api.agify.io/?name=${name}`)
+        .then((res) => res.json());
 };
 
 const App = () => {
-  const [result, setResult] = useState(null);
+    const [result, setResult] = useState(null);
+    const inputRef = useRef(null);
+    const nbRender = useRenderCount();
 
-  // 🦁 Wrap la function `onSearch` dans le hooks useDebounce
-  // 💡 const onSearch = useDebounce((value) => {...}, 500);
-  const onSearch = (value) => {
-    fetchAgeByName(value).then((data) => {
-      setResult(data);
-    });
-  };
+    const onSearch = useDebounce(() => {
+        fetchAgeByName(inputRef.current.value)
+            .then((data) => {
+                setResult(data);
+            });
+    }, 500);
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search bar"
-        onChange={(event) => {
-          onSearch(event.target.value);
-        }}
-      />
-      {result ? (
-        <div style={{ padding: 16 }}>
-          The age for <b>{result.name}</b> is <b>{result.age}</b> and there is{' '}
-          <b>{result.count}</b> people with this name.
+    return (
+        <div>
+            <h3>Nombre de render : {nbRender}</h3>
+            <input
+                ref={inputRef}
+                type="text"
+                placeholder="Search bar"
+                onChange={(event) => onSearch()}
+            />
+            {result ? (
+                <div style={{ padding: 16 }}>
+                    The age for <b>{result.name}</b> is <b>{result.age}</b> and there is{' '}
+                    <b>{result.count}</b> people with this name.
+                </div>
+            ) : null}
         </div>
-      ) : null}
-    </div>
-  );
+    );
 };
 
 export default App;
